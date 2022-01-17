e0aac10071f4407c52628325a7915b6934e3e447
DenisOuma <dennisouma38@gmail.com>
2022-01-17 07:50:40 +0300

    Chat Uplication

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+
+import './App.css'
+const projectID = '0ba8d493-ff00-4335-9d41-62a60065e285';
+
+const App = () => {
+    if (!localStorage.getItem('username')) return <LoginForm />;
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID={projectID}
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+            onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+        />
+    )
+}
+
+export default App;
